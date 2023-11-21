5abe00924095f3c2f3c5afff176f5ecb99973a7d
Nat Tabris <lumbar527@gmail.com>
2023-11-20 19:11:43 -0700

    Remove wave object

--- objects.py
+++ objects.py
@@ -13,22 +13,6 @@ class Salmon(pygame.sprite.Sprite):
 
         self.mask = pygame.mask.from_surface(self.image)
 
-        # self.image = pygame.transform.scale(self.image, (400, 800)) # Testing to see if this works. [x]
-
-        screen.blit(self.image, (x, y))
-
-class Wave(pygame.sprite.Sprite):
-    def __init__(self, image, screen, x, y):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image = image
-
-        self.image.set_colorkey((255, 255, 255))
-
-        self.rect = self.image.get_rect().move(x, y)
-
-        self.mask = pygame.mask.from_surface(self.image)
-
         screen.blit(self.image, (x, y))
 
 class Waterfall(pygame.sprite.Sprite):
